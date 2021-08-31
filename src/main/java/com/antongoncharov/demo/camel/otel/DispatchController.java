773dc170f79fc5b49456ad3c31604631fda8408c
jason <jason.irwin@idoxgroup.com>
2021-08-31 11:22:18 +0100

    Fix to MDC placeholders in log message

--- src/main/java/com/antongoncharov/demo/camel/otel/DispatchController.java
+++ src/main/java/com/antongoncharov/demo/camel/otel/DispatchController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.logging.Logger;
+
 /**
  * @author antongoncharov
  */
@@ -14,17 +16,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class DispatchController {
 
     private final ProducerTemplate template;
+    private static final Logger logger = Logger.getLogger(DispatchController.class.getName());
 
-    public DispatchController(
-        ProducerTemplate template
-    ) {
+    public DispatchController(ProducerTemplate template) {
         this.template = template;
     }
 
     @GetMapping("/dispatch")
     @ResponseBody
     public String sayHello(@RequestParam String name) {
+        logger.info("Got request for " + name);
         Object obj = template.sendBody("direct:dispatcher", ExchangePattern.InOut, name);
+        logger.info("Sent request for " + name);
         return obj.toString();
     }
 
--- src/main/java/com/antongoncharov/demo/camel/otel/DispatcherRoute.java
+++ src/main/java/com/antongoncharov/demo/camel/otel/DispatcherRoute.java
@@ -5,11 +5,14 @@ import org.apache.camel.opentelemetry.OpenTelemetryTracer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.logging.Logger;
+
 /**
  * @author antongoncharov
  */
 @Component
 public class DispatcherRoute extends RouteBuilder {
+    private static final Logger logger = Logger.getLogger(DispatcherRoute.class.getName());
 
     private final ServiceProperties serviceProperties;
 
@@ -21,20 +24,23 @@ public class DispatcherRoute extends RouteBuilder {
     @Override
     public void configure() {
 
+        logger.info("Creating tracer");
         OpenTelemetryTracer ott = new OpenTelemetryTracer();
         ott.init(this.getContext());
+        logger.info("Tracer created");
 
         from("direct:dispatcher").routeId("dispatcher-route")
+            .log("incoming request, headers = ${headers}")
             .log("incoming request, body = ${body}")
             .to("direct:sayHello");
 
         from("direct:sayHello").routeId("sayHello-route")
+            .log("outgoing request, headers = ${headers}")
+            .log("outgoing request, body = ${body}")
             .log("dispatching to /hello endpoint")
             .setHeader("status", constant("dispatched"))
             .toD("undertow:" + serviceProperties.getDownstreamEndpoint() + "?name=${body}")
             .convertBodyTo(String.class);
-
     }
-
 }
 
