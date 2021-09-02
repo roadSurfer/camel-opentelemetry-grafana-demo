aacbf4481f91d38b12f6cbd44f6544de6006e3cc
jason <jason.irwin@idoxgroup.com>
2021-09-02 09:23:29 +0100

    Aligned logging ActiveMQ branch

--- src/main/java/com/antongoncharov/demo/camel/otel/DispatcherRoute.java
+++ src/main/java/com/antongoncharov/demo/camel/otel/DispatcherRoute.java
@@ -5,15 +5,11 @@ import org.apache.camel.opentelemetry.OpenTelemetryTracer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.logging.Logger;
-
 /**
  * @author antongoncharov
  */
 @Component
 public class DispatcherRoute extends RouteBuilder {
-    private static final Logger logger = Logger.getLogger(DispatcherRoute.class.getName());
-
     private final ServiceProperties serviceProperties;
 
     @Autowired
@@ -30,7 +26,9 @@ public class DispatcherRoute extends RouteBuilder {
         from("direct:dispatcher").routeId("dispatcher-route")
             .log("incoming request, headers = ${headers}")
             .log("incoming request, body = ${body}")
-            .to("direct:sayHello");
+            .to("direct:sayHello")
+            .log("incoming response, headers = ${headers}")
+            .log("incoming response, body = ${body}");
 
         from("direct:sayHello").routeId("sayHello-route")
             .log("dispatching to /hello endpoint")
